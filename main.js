80aa4e45787979b796b4f9ba6a51567a6cde6eb3
minderov <coucoueoeu@gmail.com>
2017-10-21 21:20:22 -0700

    Added video stream

---
+++ main.js
@@ -0,0 +1,38 @@
+var width = 800;
+var height = 800;
+
+var canvas = document.createElement('canvas');
+var video = document.createElement('video');
+video.setAttribute('playsinline', '');
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.style.width = width + 'px';
+video.style.width = height + 'px';
+
+document.body.appendChild(video);
+
+/* Setting up the constraint */
+var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+var constraints = {
+    audio: false,
+    video: {
+        facingMode: facingMode
+    }
+};
+
+/* Stream it to video element */
+navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+    video.srcObject = stream;
+});
+
+function currentFrame() {
+    var context = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+
+    var data = canvas.toDataURL();
+
+    return data;
+}
+
